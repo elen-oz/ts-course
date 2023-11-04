808ac41661e235024b8cd9e830c60069f6452f35
elen-oz <ozerova.lenka@gmail.com>
2023-11-04 23:08:08 +0100

    55: Defining a Cargo

--- OAK_Academy-Team/shipment-tracking/src/index.ts
+++ OAK_Academy-Team/shipment-tracking/src/index.ts
@@ -1,3 +1,8 @@
 import { Destination } from './Destination';
+import { Cargo } from './Cargo';
+
 const destination = new Destination();
 console.log(destination);
+
+const cargo = new Cargo();
+console.log(cargo);
