79b88b08994ae430566a8ecbe6d9a315c1828181
elen-oz <ozerova.lenka@gmail.com>
2023-11-06 18:16:29 +0100

    lesson No 58

--- OAK_Academy-Team/shipment-tracking/src/index.ts
+++ OAK_Academy-Team/shipment-tracking/src/index.ts
@@ -7,4 +7,10 @@ console.log(destination);
 const cargo = new Cargo();
 console.log(cargo);
 
-google;
+new google.maps.Map(document.getElementById('g-map'), {
+  zoom: 1.6,
+  center: {
+    lat: 0,
+    lng: 20,
+  },
+});
